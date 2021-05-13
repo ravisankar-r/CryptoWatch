15e6053d2716c86cf301c1310c31de2e85c1f005
Ravisankar <ravi@paystack.com>
2021-05-13 11:43:04 +0530

    Initial Commit

---
+++ CryptoWatch/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CryptoWatch
+//
+//  Created by Ravisankar R on 13/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
